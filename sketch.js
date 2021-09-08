856fafcc2350c624d19ef33a4e77977235edd795
edegu2 <edegu2@uic.edu>
2021-09-08 00:12:26 -0500

    adding sketch done in  class, forgot to push after class.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,36 @@
 function setup() {
-    createCanvas(600, 400);
-  }
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background('black');
+  stroke('#89B089')
+  strokeWeight(4)
+  fill('#B1F1B1')
+  ellipse(200,200,200,200);
+  ellipse(200,500,400,400);
   
-  function draw() {
-    background(220, 70, 100);
-  }
+  noStroke()
+  fill('#ffffff')
+  ellipse(200,250,50,50);
+  
+  noStroke()
+  fill('#B1F1B1')
+  ellipse(200,240,50,50)
+  
+  stroke('#89B089')
+  fill('#89B089')
+  ellipse(150,150,25,25);
+  ellipse(250,150,25,25);
+  
+  noStroke(1)
+  fill('#B1F1B1')
+  ellipse(147,146,30,30);
+  ellipse(253,146,30,30);
+  
+  stroke('black')
+  fill('#262E26')
+  ellipse(160,200,20,20);
+  ellipse(240,200,20,20);
+  
+}
